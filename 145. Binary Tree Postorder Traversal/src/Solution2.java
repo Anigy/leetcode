63918fd7fbfe4775044423878959e50ff0c77374
Anigy92 <Anigy92@users.noreply.github.com>
2018-12-04 09:51:11 +0800

    145 linkedList

--- 145. Binary Tree Postorder Traversal/src/Solution2.java
+++ 145. Binary Tree Postorder Traversal/src/Solution2.java
@@ -5,8 +5,25 @@ import java.util.Objects;
 public class Solution2 {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-
-
+        if (Objects.isNull(root)) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<TreeNode> re = new Stack<>();
+        stack.push(root);
+        while (!stack.empty()) {
+            TreeNode popOne = stack.pop();
+            re.push(popOne);
+            if (Objects.nonNull(popOne.left)) {
+                stack.push(popOne.left);
+            }
+            if (Objects.nonNull(popOne.right)) {
+                stack.push(popOne.right);
+            }
+        }
+        while (!re.empty()) {
+            res.add(re.pop().val);
+        }
         return res;
     }
 }
---
+++ 145. Binary Tree Postorder Traversal/src/Solution3.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Solution3 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> res = new LinkedList<>();
+        if (Objects.isNull(root)) {
+            return res;
+        }
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        stack.addFirst(root);
+        while (!stack.isEmpty()) {
+            TreeNode popOne = stack.removeFirst();
+            res.addFirst(popOne.val);
+            if (Objects.nonNull(popOne.left)) {
+                stack.addFirst(popOne.left);
+            }
+            if (Objects.nonNull(popOne.right)) {
+                stack.addFirst(popOne.right);
+            }
+        }
+        return res;
+    }
+}
