e58c346962923e3ad0410a9d35a2e1b370db38c3
Anigy92 <Anigy92@users.noreply.github.com>
2018-12-06 01:32:43 +0800

    tree

--- 145. Binary Tree Postorder Traversal/src/Solution2.java
+++ 145. Binary Tree Postorder Traversal/src/Solution2.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Stack;
 
 public class Solution2 {
     public List<Integer> postorderTraversal(TreeNode root) {
