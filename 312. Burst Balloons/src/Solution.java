c983481a9fb54b576bafa3ea2115b866dec4e120
Anigy92 <Anigy92@users.noreply.github.com>
2019-03-20 10:02:06 +0800

    3.20.10

---
+++ 312. Burst Balloons/src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static int maxCoins(int[] nums) {
+        
+        return 1;
+    }
+
+    public static void main(String args[]) {
+        int[] nums = {3,5,1,6,2,0,8,7,4};
+        System.out.println(maxCoins(nums));
+    }
+}
