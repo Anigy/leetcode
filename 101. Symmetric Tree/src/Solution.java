5d21d5d0d53f7f835cd0040b8e5112dde7a73579
jin_yan <jin_yan@worksap.co.jp>
2019-04-15 12:12:56 +0800

    101

--- 101. Symmetric Tree/src/Solution.java
+++ 101. Symmetric Tree/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Solution {
@@ -15,5 +17,59 @@ public class Solution {
         }else  {
             return left.val == right.val && checkSame(left.left, right.right) && checkSame(left.right, right.left);
         }
+    }
+
+    public static boolean isSymmetricRecursive(TreeNode root) {
+        if (Objects.isNull(root)) {
+            return true;
+        }
+        TreeNode iter = root;
+        List<TreeNode> queuen = new ArrayList<>();
+        List<TreeNode> tempQueuen = new ArrayList<>();
+        queuen.add(root);
+        while (queuen.size() > 0) {
+            boolean flag = false;
+            tempQueuen.clear();
+            for (TreeNode node : queuen) {
+                if (Objects.isNull(node)) {
+                    tempQueuen.add(null);
+                    tempQueuen.add(null);
+                } else {
+                    flag = true;
+                    tempQueuen.add(node.left);
+                    tempQueuen.add(node.right);
+                }
+            }
+            if(!flag) {
+                return true;
+            }else if (!isValidatedQ(tempQueuen)) {
+                return false;
+            }
+            queuen.clear();
+            queuen.addAll(tempQueuen);
+        }
+        return false;
+    }
+
+    private static boolean isValidatedQ(List<TreeNode> queuen) {
+        if (queuen.size() == 0) {
+            return true;
+        } else if (queuen.size() % 2 != 0) {
+            return false;
+        }
+        int l = 0;
+        int r = queuen.size() - 1;
+        while (l < r) {
+            if ((Objects.isNull(queuen.get(l)) && Objects.nonNull(queuen.get(r)))
+                    || (Objects.nonNull(queuen.get(l)) && Objects.isNull(queuen.get(r)))
+                    || (Objects.nonNull(queuen.get(l))
+                    && Objects.nonNull(queuen.get(r))
+                    && queuen.get(l).val != queuen.get(r).val)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
     }
 }
